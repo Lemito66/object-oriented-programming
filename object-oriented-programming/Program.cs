ebde030314eaa4c5cff709e9d68525ed7d63d4d9
Lemito66 <lemito66@gmail.com>
2024-10-29 10:37:08 -0500

    creación de una clase en C#

---
+++ object-oriented-programming/Program.cs
@@ -0,0 +1,26 @@
+﻿// Programación Orientada a Objetos
+
+// Es un paradigma de programación que se basa en el uso de clases y objetos.
+
+var beer = new Beer
+{
+    Name = "Stella Artois",
+    Brand = "AB InBev",
+    Alcohol = 5.0,
+    Price = 2.5m
+};
+
+Console.WriteLine(beer.Name);
+
+
+
+public class Beer
+{
+
+   public string Name { get; set; }
+   public string Brand { get; set; }
+   public double Alcohol { get; set; }
+   public decimal Price { get; set; }
+}
+
+
